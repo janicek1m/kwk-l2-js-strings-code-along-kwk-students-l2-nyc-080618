20dce0ebb85848114c1fc0b4d110131b9c81f9be
Janice Kim <janicekim60@gmail.com>
2018-08-08 14:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = "Janice"
 const welcomeMessage = 'Welcome to Klossybook, ' + currentUser + "!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = "Welcome, " + currentUser(0)
+const shortGreeting = "Welcome, " + currentUser[0]
