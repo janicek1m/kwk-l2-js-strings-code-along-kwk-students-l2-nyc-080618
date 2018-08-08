98b1c6fa6e9efb57f7b01a529f814b85d1495dc2
Janice Kim <janicekim60@gmail.com>
2018-08-08 14:11:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Janice"
 const welcomeMessage = 'Welcome to Klossybook, ' + currentUser + "!";
-const excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage
